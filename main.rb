1e5d056d5feeaaf32d516b7c925cd376a2451799
xXxRosexXxEE <rose@the-flower-garden.co.uk>
2013-11-28 16:13:35 +0000

    Updated Ruby Code with put pixel

--- main.rb
+++ main.rb
@@ -1,21 +1,42 @@
 #!/usr/bin/ruby
 require 'gosu'
 require 'rubygems'
+require 'texplay'
+
 
 class Window < Gosu::Window
 	def initialize
 			puts "Hello"
-			super 640,480, false
+			super(640,480, false, 20)
 			self.caption = "Test Window"
+			@pixel_surface = TexPlay::create_blank_image(self, 640,480)
+			$test = 100;
+			
+			puts $test.ceil >= 10
+		
 	end
 	
 	def update
 	end
 	
 	def draw
+	
+	@pixel_surface.paint {
+	
+	100.times do |x|
+		100.times do |y|
+	
+		pixel x*2,y*2, :color => [0,1,0,1]
+			
+		end
+	end
+	}
+	
+	@pixel_surface.draw(0,0,0)
 	end
 end
 	MainWindow = Window.new
 	MainWindow.show
+	
 	
 	
